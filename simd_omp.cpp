bdc51bb05292fbb6735eb6ffa33bea706420ac37
Dzu Nguyen <dustinwind@Dzus-MacBook-Pro.local>
2025-08-31 00:43:37 -0400

    Fix simd_omp

--- simd_omp.cpp
+++ simd_omp.cpp
@@ -6,10 +6,10 @@
 
 #include <random>
 
-void generateRandom(int *arr, size_t size)
+void generateRandom(char *arr, size_t size)
 {
     std::mt19937 rng(std::random_device{}());
-    std::uniform_int_distribution<int> dist(0, 99);
+    std::uniform_int_distribution<int> dist(0, 2);
     for (size_t i = 0; i < size; ++i)
     {
         arr[i] = dist(rng);
@@ -18,28 +18,28 @@ void generateRandom(int *arr, size_t size)
 
 int main()
 {
-    const int N = 1000;
-    int a[N] = {};
-    int b[N] = {};
-    int c[N] = {};
+    const long N = 1000000;
+    char a[N] = {};
+    char b[N] = {};
+    char c[N] = {};
 
-    generateRandom(a, N);
     generateRandom(b, N);
     generateRandom(c, N);
 
     auto start = std::chrono::high_resolution_clock::now();
-    // #pragma omp simd
+    #pragma omp simd
     for (int i = 0; i < N; ++i)
     {
         a[i] = b[i] + c[i];
     }
     auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::nano> duration = end - start;
-    std::cout << "duration: " << duration.count() << " ns" << "\n";
+    std::chrono::duration<double, std::micro> duration = end - start;
+    std::cout << "duration: " << duration.count() << " micro s" << "\n";
 
+    // This will remove optimization which the compiler knows a[i]
+    // is never used so it is never calculated.
     for (int i = 0; i < 10; ++i)
     {
-        a[i] = b[i] + c[i];
         std::cout << a[i] << "\n";
     }
 }
