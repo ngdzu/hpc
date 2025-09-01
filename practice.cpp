adf1b804a41d763f2beaad81a64db38ed44cffb4
Dzu Nguyen <dustinwind@Dzus-MacBook-Pro.local>
2025-08-31 22:23:28 -0400

    add practice file

---
+++ practice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+#include <chrono>
+#include <random>
+int main()
+{
+
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution<int> dist(1, 3);
+    int random_number = dist(rng);
+    const int N = 10000;
+
+    // std::vector<int> a(N, 0);
+    long long sum = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    // #pragma omp parallel for reduction(+ : sum)
+    for (int i = 1; i <= N; ++i)
+    {
+        sum += random_number;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float, std::micro> duration = end - start;
+    std::cout << "Sum: " << sum << "\n";
+    std::cout << "duration: " << duration.count() << " micro secs\n";
+
+    return 0;
+}
